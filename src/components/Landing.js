9458cd4de05d90ae0c94f042f79c5991faf68e30
Peggyta <peggyta9096@gmail.com>
2022-10-03 00:09:07 +0330

    using filter method for search

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,20 +7,27 @@ import Coin from './Coin';
 
 const Landing = () => {
     const[coins, setCoins] = useState([]);
+    const[search, setSearch] = useState("");
     useEffect(() => {
         const fetchAPI = async() => {
             setCoins(await getCoin());
         }
         fetchAPI()
     }, [])
+
+const searchHandler = event => {
+    setSearch(event.target.value)
+}
+const searchedCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
+
     return (
         <>
-            <input type="text" placeholder="search..." />
+            <input type="text" placeholder="search..." value={search} onChange={searchHandler} />
             {
                 coins.length ? 
                 <div>
                     {
-                        coins.map (coin => <Coin 
+                        searchedCoins.map (coin => <Coin 
                             key={coin.id}
                             name={coin.name}
                             image={coin.image}
