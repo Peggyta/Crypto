7a4b403191511c1acc78746111d2704657b42de4
Peggyta <peggyta9096@gmail.com>
2022-10-04 14:54:03 +0330

    a crypto currency project

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,12 @@
 import React from 'react';
 //Gif
 import spinner from './gif/spinner.gif';
+//Style
+import styles from './Loading.module.css';
 
 const Loading = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <img src={spinner} alt="loading" />
             <h2>Loading ...</h2>
         </div>
