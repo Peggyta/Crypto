49223e8b6df1d0a77238060baf1579357dba3473
Peggyta <peggyta9096@gmail.com>
2022-10-07 15:00:12 +0330

    carousel styles added

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,8 +1,12 @@
 import React, {useEffect, useState} from 'react';
+import AliceCarousel from 'react-alice-carousel';
 //Components
 import CoinIndex from './CoinIndex';
 //API
 import { indexCoin } from '../services/api';
+//Styles
+import styles from './Carousel.module.css';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 const Carousel = () => {
     const[trending, setTrending] = useState([]);
@@ -12,14 +16,17 @@ const Carousel = () => {
         }
         fetchAPI()
     },[])
+
+
     return (
-        <div>
-            {
-                trending.map(coin => <CoinIndex key={coin.id} 
+        <div className={styles.container}>
+             {
+                    trending.map(coin => <CoinIndex key={coin.id} 
                     image={coin.image}
                     symbol={coin.symbol}
                     price={coin.current_price} />)
-            }
+                    }
+            
         </div>
     );
 };
--- src/components/CoinIndex.js
+++ src/components/CoinIndex.js
@@ -1,13 +1,15 @@
 import React from 'react';
 //Styles
-
+import styles from './CoinIndex.module.css';
 
 const CoinIndex = ({image, price, symbol}) => {
     return (
-        <div >
-            <img src={image} alt="coins" style={{width:"100px"}} />
-            <span>{symbol}</span> 
-            <span>$ {price.toLocaleString()}</span>    
+        <div className={styles.container} >
+            <img src={image} alt="coins" />
+            <div className={styles.textContainer}>
+                <span className={styles.symbol}>{symbol}</span> 
+                <span className={styles.price}>{price.toLocaleString()} $</span>
+             </div>
         </div>
     );
 };
